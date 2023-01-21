123ff8441fc011bfbced5325b71ffdd85e8851b7
Nikoloz97 <nick.gotsy@gmail.com>
2023-01-20 19:45:34 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Scanner class = C# stream reader
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Hey");
+        System.out.print(" There");
+        System.out.println("Hello World!");
+
+        System.out.println("This is a test");
+
+
+    }
+}
